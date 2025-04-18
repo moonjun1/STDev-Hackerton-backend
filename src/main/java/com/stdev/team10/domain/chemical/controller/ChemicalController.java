58d32eba1d07c11deb45bd2e930c4e2e1a4975d2
june <0721hjp@naver.com>
2025-04-18 23:12:22 +0900

    chore: data / data1 파일 삭제

--- src/main/java/com/stdev/team10/domain/chemical/controller/ChemicalController.java
+++ src/main/java/com/stdev/team10/domain/chemical/controller/ChemicalController.java
@@ -1,6 +1,7 @@
 package com.stdev.team10.domain.chemical.controller;
 
 import com.stdev.team10.domain.chemical.service.ChemicalService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,11 +19,13 @@ public class ChemicalController {
     @Autowired
     private ChemicalService chemicalService;
 
+    @Operation(summary = "물질 전부 조회 api", description = "모든 물질 정보를 조회합니다.")
     @GetMapping("/find/all")
     public ResponseEntity<?> chemicalFindAll() {
         return chemicalService.chemicalFindAll();
     }
 
+    @Operation(summary = "화학식으로 특정 물질 정보 조회 api", description = "화학식으로 특정 물질 정보를 조회합니다.")
     @GetMapping("/find/{molecularFormula}")
     public ResponseEntity<?> chemicalFindByMolecularFormula(@PathVariable String molecularFormula) {
         return chemicalService.chemicalFindByMolecularFormula(molecularFormula);
--- src/main/java/com/stdev/team10/global/common/data/ChemicalData.java
+++
@@ -1,7 +0,0 @@
-package com.stdev.team10.global.common.data;
-
-import java.util.List;
-
-public class ChemicalData {
-    private List<String> chemicals;
-}
